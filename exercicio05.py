91c8c316914cf3cc1f79dcc13b44e8d0a8b69f57
thomthomtt <thomazgt39@gmail.com>
2025-04-15 16:53:23 -0300

    media pela quantidade de notas

---
+++ exercicio05.py
@@ -0,0 +1,11 @@
+i=0
+var=0
+while i>2:
+    i+=1
+    emput=int(input("Digite um número: "))
+    while emput==0:
+        imput_zero=int(input("O valor inserido não corresponde, digite outro: "))
+    var=emput
+div = var/emput
+print(f"{div}")
+
